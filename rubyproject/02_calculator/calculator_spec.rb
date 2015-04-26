3ae94928a31818c09987a45bd6f5b8a9e0dfc10f
kriox26 <matias.pan26@gmail.com>
2015-04-26 10:58:52 -0300

    Set proper indentation for ruby files

--- rubyproject/02_calculator/calculator_spec.rb
+++ rubyproject/02_calculator/calculator_spec.rb
@@ -80,36 +80,36 @@ end
 describe "#multiply" do
 
   it "multiplies two numbers" do 
-	  multiply([3,4]).should == 12
+    multiply([3,4]).should == 12
   end
 
   it "multiplies several numbers" do 
-	  multiply([1,3,6,7]).should == 126
+    multiply([1,3,6,7]).should == 126
   end
 
 end
 
 describe "#power" do
   it "raises one number to the power of another number" do 
-	  power(2,2).should == 4
+    power(2,2).should == 4
   end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
   it "computes the factorial of 0" do 
-	  factorial(0).should == 1
+    factorial(0).should == 1
   end
   it "computes the factorial of 1" do 
-	  factorial(1).should == 1
+    factorial(1).should == 1
   end
   it "computes the factorial of 2" do 
-	  factorial(2).should == 2
+    factorial(2).should == 2
   end
   it "computes the factorial of 5" do 
-	  factorial(5).should == 120
+    factorial(5).should == 120
   end
   it "computes the factorial of 10" do 
-	  factorial(10).should == 3628800
+    factorial(10).should == 3628800
   end
 end
