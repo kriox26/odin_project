24a242ca295dbc71013000df79e3c5c0cb800368
kriox26 <matias.pan26@gmail.com>
2015-05-16 14:36:59 -0300

    add useful comments

--- ruby_building_blocks/caesar.rb
+++ ruby_building_blocks/caesar.rb
@@ -1,10 +1,11 @@
 # Ruby building blocks, first project of Ruby programming course from odin_project
 # Matias Pan - https://github.com/kriox26/
 #
-#Implement this two methods for checking if a character is lower or upper case
+# Implement this two methods for checking if a character is lower or upper case
   def is_upper?(c)
     return c == c.capitalize
   end
+  # Actually i only use one, but i implement both just in case
   def is_lower?(c)
     return c != c.capitalize
   end
@@ -12,22 +13,22 @@
   def letter?(lookAhead)
     lookAhead =~ /[A-Za-z]/
   end
-# Function for doing the shift of a letter
+# parse_letter returns the letter shifted key places
   def parse_letter(from_where, letter, key)
     # Check if you go from Z to A or z to a
     return (letter.ord + key > from_where)?((letter.ord + key - 26).chr):((letter.ord + key).chr)
   end
 
-# Get from the command line the encryption key
+# Get encryption key from command line
 key = ARGV.first
 key = key.to_i
-# just in case the size of the key is bigger than 26
+# this only matters when key > 26, for example, key = 27 --> key = 27 % 26 = 1
   key = key % 26
-#Get string from user
+# Get string from user
 print "String to encrypt: "
 string = STDIN.gets.chomp()
 string.length().times do |i|
-  # If is not a letter then we dont shift 
+  # If the char is not a letter, we leave it the way it is, it's how caesar works
   if letter?(string[i])
     if is_upper?(string[i])
       # call to parse_letter with upcase, we do this to mantain their case
@@ -39,4 +40,4 @@ string.length().times do |i|
   end 
 end
 # Print the encrypted string
-puts string
+puts "Encryption: #{string}"
