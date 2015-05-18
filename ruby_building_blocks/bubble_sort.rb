4a9aa58fc289eb7b62559e889364745f85d6054e
kriox26 <matias.pan26@gmail.com>
2015-05-18 17:11:21 -0300

    done with bubble_sort_by

--- ruby_building_blocks/bubble_sort.rb
+++ ruby_building_blocks/bubble_sort.rb
@@ -21,6 +21,28 @@ def bubble_sort(array_to_sort)
   array_to_sort                 # Return the sorted array
 end
 
+def bubble_sort_by(array_to_sort)
+  last_swap = 1 # We set it to 1 only to force it to do at least one iteration
+  while last_swap > 0
+    new_swap = 0
+    (array_to_sort.length()-1).times do |i|
+      if yield(array_to_sort[i], array_to_sort[i+1]) > 0
+        array_to_sort[i+1], array_to_sort[i] = swap(array_to_sort[i+1], array_to_sort[i])
+        new_swap = i + 1 # Update the swap we just did
+      end
+    end
+    last_swap = new_swap        # Update last_swap to the swap latest swap of the for
+  end
+  array_to_sort                 # Return the sorted array
+end
+
 array = [4,3,78,2,0,2]
 sorted_array = bubble_sort(array)
 p sorted_array
+
+new_array = ["hi","hello","hey"]
+sorted_array = bubble_sort_by(new_array) do |left,right|
+  left.length - right.length
+end
+p sorted_array
+
