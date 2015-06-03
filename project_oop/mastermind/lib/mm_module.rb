ed9bd3e83acc285d3efbf6c9eb3fe48958902afb
kriox26 <matias.pan26@gmail.com>
2015-06-03 00:41:08 -0300

    re do rate_guess

--- project_oop/mastermind/lib/mm_module.rb
+++ project_oop/mastermind/lib/mm_module.rb
@@ -5,9 +5,9 @@ module Mastermind
   COLORS = [:blue, :red, :white, :yellow, :green, :magenta]
 
   SPLIT = { vertical:  "\u2551", horizontal: "\u2550", cross:     "\u256c",
-	    nw_corner: "\u2554", ne_corner:  "\u2557", se_corner: "\u255d",
-	    sw_corner: "\u255a", n_divide:   "\u2566", s_divide:  "\u2569",
-	    e_divide:  "\u2563", w_divide:   "\u2560" }
+			nw_corner: "\u2554", ne_corner:  "\u2557", se_corner: "\u255d",
+			sw_corner: "\u255a", n_divide:   "\u2566", s_divide:  "\u2569",
+			e_divide:  "\u2563", w_divide:   "\u2560" }
 
   WELCOME_MESSAGE = <<-WELCOME
   Hello there! Mastermind is a two players game, in this game you will be playing against some famous AI!(like Skynet). You first decide 
@@ -23,160 +23,159 @@ module Mastermind
   WELCOME
 
   def print_top_message(is_ai,name_of_ai)
-    system "clear"
-    puts "\t\t\t\tMastermind Game".colorize(:color=>:light_white,:background=>:light_magenta)
-    puts
-    if is_ai
-      puts "#{ name_of_ai } is playing!!"
-    else
-      puts "You are playing!"
-      puts "Colors available: #{'       '.colorize(:background=>COLORS[0])}#{ '       '.colorize(:background=>COLORS[1])}#{ '       '.colorize(:background=>COLORS[2]) }#{ '       '.colorize(:background=>COLORS[3]) }#{ '       '.colorize(:background=>COLORS[4]) }#{ '       '.colorize(:background=>COLORS[5]) }"
-      puts "Choose with:        blue   red   white  yellow green magenta"
-    end
-    puts
-    puts "\t\t       #{ 'Guess board'.colorize(:light_red) }\t    #{ 'Score board'.colorize(:light_red) }"
+	system "clear"
+	puts "\t\t\t\tMastermind Game".colorize(:color=>:light_white,:background=>:light_magenta)
+	puts
+	if is_ai
+	  puts "#{ name_of_ai } is playing!!"
+	else
+	  puts "You are playing!"
+	  puts "Colors available: #{'       '.colorize(:background=>COLORS[0])}#{ '       '.colorize(:background=>COLORS[1])}#{ '       '.colorize(:background=>COLORS[2]) }#{ '       '.colorize(:background=>COLORS[3]) }#{ '       '.colorize(:background=>COLORS[4]) }#{ '       '.colorize(:background=>COLORS[5]) }"
+	  puts "Choose with:        blue   red   white  yellow green magenta"
+	end
+	puts
+	puts "\t\t       #{ 'Guess board'.colorize(:light_red) }\t    #{ 'Score board'.colorize(:light_red) }"
   end
 
   def generate_secret_code
-    secret_code = [COLORS[rand(6)], COLORS[rand(6)], COLORS[rand(6)], COLORS[rand(6)]]
-    return secret_code
+	secret_code = [COLORS[rand(6)], COLORS[rand(6)], COLORS[rand(6)], COLORS[rand(6)]]
+	return secret_code
   end
 
   def input_secret_code(name)
-    print_top_message(false,'name')
-    puts "Hey #{ name }. Enter secret code as a comma separated list(e.g: r,b,w,m): "
-    loop do
-      secret_code = gets.chomp.split.join.split(',').map(&:to_sym)
-      return secret_code if check_code(secret_code)
+	print_top_message(false,'name')
+	puts "Hey #{ name }. Enter secret code as a comma separated list(e.g: r,b,w,m): "
+	loop do
+	  secret_code = gets.chomp.split.join.split(',').map(&:to_sym)
+	  return secret_code if check_code(secret_code)
 	  puts 'That\'s not a valid code, try again, and remember the example: '.colorize(:red)
-    end
+	end
   end
 
   def check_code(code)
-    # Checks if each color in guess is available in COLORS
-    if code == [] || code.length < 4
-      return false
-    end
-    parse_colors(code)
-    code.each do |element|
-      return false if !(COLORS.include?(element))
-    end
-    return true
+	# Checks if each color in guess is available in COLORS
+	if code == [] || code.length < 4
+	  return false
+	end
+	parse_colors(code)
+	code.each do |element|
+	  return false if !(COLORS.include?(element))
+	end
+	return true
   end
 
   def parse_colors(code)
-    code.map! do |color|
-      if color == :r
-	:red
-      elsif color == :b
-	:blue
-      elsif color == :w
-	:white
-      elsif color == :g
-	:green
-      elsif color == :y
-	:yellow
-      elsif color == :m
-	:magenta
-      else
-	color
-      end
-    end
+	code.map! do |color|
+	  if color == :r
+		:red
+	  elsif color == :b
+		:blue
+	  elsif color == :w
+		:white
+	  elsif color == :g
+		:green
+	  elsif color == :y
+		:yellow
+	  elsif color == :m
+		:magenta
+	  else
+		color
+	  end
+	end
   end
 
   def rate_guess(guess,secret_code,from_ai=false)
-    i=0
-    score = []
-    # for keeping tracks of whites
-    guess_no_match = Array.new()
-    secret_no_match = Array.new()
-    guess.each_with_index do |color,index|
-      if secret_code[index] == color
-	# Right color in right position
-	score[i]=:black
-	i+=1
-      else
-	guess_no_match << color
-	secret_no_match << secret_code[index]
-      end
-    end
-    guess_no_match.each do |color|
-      nodp = secret_no_match.index(color)
-      unless nodp.nil?
-	secret_no_match.delete_at(nodp)
-	score[i]=:white
-	i+=1
-      end
-    end
-    if !from_ai
-      return score,secret_code==guess
-    else
-      return score
-    end
+	i=0
+	score = []
+	guess_no_match = []
+	secret_no_match = []
+	guess.each_with_index do |color,index|
+	  if secret_code[index] == color
+		# Right color in right position
+		score[i]=:black
+		i+=1
+	  else
+		guess_no_match << color
+		secret_no_match << secret_code[index]
+	  end
+	end
+	guess_no_match.each do |color|
+	  nodp = secret_no_match.index(color)
+	  unless nodp.nil?
+		secret_no_match.delete_at(nodp)
+		score[i]=:white
+		i+=1
+	  end
+	end
+	if !from_ai
+	  return score,secret_code==guess
+	else
+	  return score
+	end
   end
 
   def message_until_enter(message)
-    catch :press_enter do
+	catch :press_enter do
 	  loop do 
 		puts message
 		enter = gets
-	    throw :press_enter if enter == "\n"
+		throw :press_enter if enter == "\n"
 	  end
 	end
   end
 
   def print_secret_code(code)
-    print "\t\t"
-    print SPLIT[:nw_corner]
-    4.times do |i|
-      5.times do
-	print SPLIT[:horizontal]
-      end
-      if i == 3
-	print SPLIT[:ne_corner]
-      else
-	print SPLIT[:n_divide]
-      end
-    end
-    puts
-    print "\t\t"
-    print SPLIT[:vertical]
-    4.times do |j|
-      print ' '
-      print '   '.colorize(:background => code[j])
-      print ' '
-      print SPLIT[:vertical]
-    end
-    puts
-    print "\t\t"
-    print SPLIT[:sw_corner]
-    4.times do |k|
-      5.times do
-	print SPLIT[:horizontal]
-      end
-      if k==3
-	print SPLIT[:se_corner]
-      else
-	print SPLIT[:s_divide]
-      end
-    end
-    puts
+	print "\t\t"
+	print SPLIT[:nw_corner]
+	4.times do |i|
+	  5.times do
+		print SPLIT[:horizontal]
+	  end
+	  if i == 3
+		print SPLIT[:ne_corner]
+	  else
+		print SPLIT[:n_divide]
+	  end
+	end
+	puts
+	print "\t\t"
+	print SPLIT[:vertical]
+	4.times do |j|
+	  print ' '
+	  print '   '.colorize(:background => code[j])
+	  print ' '
+	  print SPLIT[:vertical]
+	end
+	puts
+	print "\t\t"
+	print SPLIT[:sw_corner]
+	4.times do |k|
+	  5.times do
+		print SPLIT[:horizontal]
+	  end
+	  if k==3
+		print SPLIT[:se_corner]
+	  else
+		print SPLIT[:s_divide]
+	  end
+	end
+	puts
   end
 
-  # use this when updating last_rating of ai player
+  # only used when updating last rating of ai player
   def number_of_wb(array)
-    whites = 0
-    blacks = 0
-    array.each do |element|
-      if element == :black
-        blacks+=1
-      else
-	if element == :white
-	  whites+=1
+	whites = 0
+	blacks = 0
+	array.each do |element|
+	  if element == :black
+		blacks+=1
+	  else
+		if element == :white
+		  whites+=1
+		end
+	  end
 	end
-      end
-    end
-    return [whites,blacks]
+	return [whites,blacks]
   end
 
 end
