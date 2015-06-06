3a4927c30303cfc445437c1bbd961f91f624a35a
kriox26 <matias.pan26@gmail.com>
2015-06-06 17:04:32 -0300

    add functions that where in the class

--- project_serializing/hangman/lib/hangman_module.rb
+++ project_serializing/hangman/lib/hangman_module.rb
@@ -12,6 +12,13 @@ and you will return to state of that game.
 Have fun!
 
   MENU
+  INPUT_MESSAGE = <<-INPUT
+These are the options you have:
+  #{'*'.colorize(:light_blue)} Type a word or character(case sensitive), if the word is correct, the game ends, if the character is correct right guesses is updated. Else you lose a life.
+  #{'*'.colorize(:light_blue)} If you want to save the game and then quit type: save
+  #{'*'.colorize(:light_blue)} If you want to quit without saving type: quit
+
+  INPUT
 
   def display(some_array)
 	some_array.each do |elem|
@@ -21,6 +28,52 @@ Have fun!
 
   def is_letter?(char)
     char =~ /[A-Za-z]/
+  end
+
+  def check_input(input)
+	if input.length == 1
+	  return true if is_letter?(input)
+	else
+	  return true if input[/[a-zA-Z]+/] == input
+	end
+	puts "Wrong input, it must be a letter or a word with only letters. Try again".colorize(:red)
+	return false
+  end
+
+  def get_name_to_save
+	name = player_name + ".yml"
+	loop do
+	  break if !File.exists?(name)
+	  name = player_name + rand(100).to_s + ".yml"
+	end
+	return name
+  end
+
+  def get_games_available
+	games = Dir["./*.yml"] 
+	if games != []
+	  games.collect! do |elem|
+		elem.gsub!(/\.\//, '')
+		elem.gsub!(/\.yml/, '')
+	  end
+	end
+	return games
+  end
+
+  def name_message(message)
+    print message
+	name = gets.chomp
+	name = name + ".yml"
+	return name
+  end
+
+  def delete(name)
+	File.delete(name)
+  end
+
+  def quit_game
+	puts "Bye!!".colorize(:magenta)
+	exit
   end
 end
 
