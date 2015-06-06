bbf521bb1a7fb3197e88afc8ad102d501f9886d4
kriox26 <matias.pan26@gmail.com>
2015-06-06 00:52:28 -0300

    re-implement #display_game_board, should work now

--- project_serializing/hangman/lib/display_hangman.rb
+++ project_serializing/hangman/lib/display_hangman.rb
@@ -1,36 +1,31 @@
 class HangmanDisplay
-  PIECES = ["☹" , "╱" ,"╿" , "╲" , "╱" , "╲" ]
-  SEPARATORS = { vertical: "⎹", top:"ᒥ⎺⎺⎺ᒣ" , bottom:"ーー"  }
+  PIECES = ["⎹" , "⎹" , "⎹" , "ᒥ" , "⎺" , "⎺" , "⎺" , "ᒣ" , "☹" , "╱" ,"╿" , "╲" , "╱" , "╲" ]
+  SEPARATORS = { bottom:"‾‾‾‾" , bottom2:"ーー"  }
 
   def initialize
-	@hanging = Array.new(6," ")
+	@hanging = Array.new(14," ")
   end
 
-  def display_game_board(right_chars, wrong_chars, moves)
-	if (10 - wrong_chars.length) <= 6
-	  update_dead_man((wrong_chars.length - 4))
-	end
+  def display_game_board(right_chars, wrong_chars)
+	update_dead_man(wrong_chars.length)
 	system("clear")
 	puts "\t" + "Hangman".colorize(:color => :light_white , :background => :light_magenta)
-	display_top_line
-	display_center
+	display_figure
 	display_bottom
-	display_table(right_chars,wrong_chars, moves)
+	display_table(right_chars,wrong_chars)
   end
 
+  private
   def update_dead_man(size)
 	for i in 0..(size - 1)
 	  @hanging[i] = PIECES[i]
 	end
   end
-  
-  def display_top_line
-    puts "\t #{SEPARATORS[:top]}"
-  end
-  def display_center
-    puts "\t#{SEPARATORS[:vertical]}    #{@hanging[0]}"
-	puts "\t#{SEPARATORS[:vertical]}   #{@hanging[1]}#{@hanging[2]}#{@hanging[3]}"
-	puts "\t#{SEPARATORS[:vertical]}   #{@hanging[4]} #{@hanging[5]}"
+  def display_figure
+	puts "\t #{@hanging[3]}#{@hanging[4]}#{@hanging[5]}#{@hanging[6]}#{@hanging[7]}"
+    puts "\t#{@hanging[2]}    #{@hanging[8]}"
+	puts "\t#{@hanging[1]}   #{@hanging[9]}#{@hanging[10]}#{@hanging[11]}"
+	puts "\t#{@hanging[0]}   #{@hanging[12]} #{@hanging[13]}"
   end
   def display_bottom
     puts "\t#{SEPARATORS[:bottom]}"
@@ -40,16 +35,16 @@ class HangmanDisplay
 	  print " #{elem}"
 	end
   end
-  def display_table(right_chars,wrong_chars, moves)
+  def display_table(right_chars,wrong_chars)
 	# display table with information, moves remaining are @hanging.length - wrong_chars.length
-	print "Word completion:".colorize(:green)  
+	print "Right guesses:".colorize(:green)  
 	display(right_chars)
 	puts
 	print "Wrong guesses:".colorize(:red) 
 	display(wrong_chars)
 	puts
-	print "Moves remaining: " 
-	print (10 - moves)
+	print "Moves remaining:".colorize(:blue)
+	print " #{(14 - wrong_chars.length)}"
 	puts
   end
 end
