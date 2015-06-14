04a7863760532f055a035396d0d6482a915fd2c6
kriox26 <matias.pan26@gmail.com>
2015-06-13 23:36:31 -0300

    reimplemented in a more efficient way

--- project_recursion/merge.rb
+++ project_recursion/merge.rb
@@ -5,19 +5,11 @@ def merge_sort(array)
 end
 
 def merge_arrays(left, right)
-  results = []
-  last_l, last_r = 0, 0
-  while (last_l < left.length) && (last_r < right.length)
-	if left[last_l] < right[last_r]
-	  results << left[last_l]
-	  last_l += 1
-	else 
-	  results << right[last_r]
-	  last_r += 1
-	end
+  result = []
+  until left.empty? or right.empty?
+	left.first <= right.first ? result << left.shift : result << right.shift
   end
-  results.concat(left[last_l..-1])
-  results.concat(right[last_r..-1])
+  result + left + right
 end
 
 p merge_sort([9,5,0,0,1,-3,11,13,29,1000,24,15])
