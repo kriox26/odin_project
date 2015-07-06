279a38d27f1ff112be289aaf026d9f9c38c302d4
kriox26 <matias.pan26@gmail.com>
2015-07-05 23:17:23 -0300

    \#clean_board implemented

--- project_testing/connect_four/lib/board.rb
+++ project_testing/connect_four/lib/board.rb
@@ -1,8 +1,17 @@
 class Board
   ROWS = 6 # ROWS = x
   COLS = 7 # COLS = y
+
   attr_accessor :board
+
   def initialize
 	@board = Array.new(6)  { Array.new(7, '  ') }
   end
+
+  def clean_board
+	if !(@board.count('  ') == @board.size)
+	  @board.map! { |elem| elem = '  ' }
+	end
+  end
+
 end
