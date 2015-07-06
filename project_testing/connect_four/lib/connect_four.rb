71646fe1fca7bb270727afb3985f261af1e3c290
kriox26 <matias.pan26@gmail.com>
2015-07-05 22:59:08 -0300

    initial code for passing test

--- project_testing/connect_four/lib/connect_four.rb
+++ project_testing/connect_four/lib/connect_four.rb
@@ -11,4 +11,29 @@ class ConnectFour
   def start
 	puts "Welcome to Connect Four game!"
   end
+
+  def get_move
+	loop do
+	  move = user_input
+	  return (move.to_i - 1) if correct_input?(move)
+	end
+  end
+
+  def user_input
+	loop do
+	  input = gets.chomp
+	  return input if !input.empty?
+	end
+  end
+
+  def correct_input?(inp)
+	inpx = inp.to_i
+	return true if inpx.between?(1,Board::COLS)
+	false
+  end
+
+  def switch_player
+	@p_playing = @p_playing == PLAYERS.first ? PLAYERS.last : PLAYERS.first
+  end
+
 end
