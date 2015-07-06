6dd2600a23df2560fbce1f8ccec6bb1112a86b50
kriox26 <matias.pan26@gmail.com>
2015-07-05 22:58:51 -0300

    couple of specs for connect four game

--- project_testing/connect_four/spec/cf_spec.rb
+++ project_testing/connect_four/spec/cf_spec.rb
@@ -4,6 +4,9 @@
 
 describe ConnectFour do
   let(:game) { ConnectFour.new }
+  let(:p1) { ConnectFour::PLAYERS.first }
+  let(:p2) { ConnectFour::PLAYERS.last }
+
   before(:example) do
 	allow(game).to receive(:puts)
 	allow(game).to receive(:print)
@@ -31,14 +34,62 @@ describe ConnectFour do
 
   end
 
-  context "#play" do
-
-	it "" do
+  context "#user_input" do
+	before(:example) do
+	  allow(game).to receive(:gets).and_return("",'',"10")
 	end
 
-	it "" do
+	it "rejects empty input" do
+	  expect(game.user_input).to eq("10")
 	end
 
+	it "accepts a non empty input" do
+	  expect(game.user_input).to eq("10")
+	end
+  end
+
+  context "#correct_input?" do
+
+	it "rejects invalid input" do
+	  expect(game.correct_input?(-2)).to be false
+	  expect(game.correct_input?("hello")).to be false
+	end
+
+	it "accepts valid positions" do
+	  expect(game.correct_input?(5)).to be true
+	end
+
+  end
+
+  context "#get_move" do
+	before(:example) do
+	  allow(game).to receive(:user_input).and_return("input", 0, 21, 3)
+	end
+
+	it "rejects invalid move" do
+	  expect(game.get_move).to eq(2)
+	end
+
+	it "accepts valid move" do
+	  expect(game.get_move).to eq(2)
+	end
+
+  end
+
+  context "#switch_player" do
+
+	it "switches from player 1 to player 2" do
+	  expect(game.p_playing).to eq(p1)
+	  game.switch_player
+	  expect(game.p_playing).to eq(p2)
+	end
+
+	it "switches from player 2 to player 1" do
+	  game.switch_player
+	  expect(game.p_playing).to eq(p2)
+	  game.switch_player
+	  expect(game.p_playing).to eq(p1)
+	end
   end
 
 end
